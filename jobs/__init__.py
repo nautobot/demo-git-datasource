2357c2ab21f3cce6aa6043beba4e64b08d8712c0
Glenn Matthews <glenn.matthews@networktocode.com>
2023-06-05 15:32:49 -0400

    Fix absolute paths

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,6 +1,6 @@
 from nautobot.core.celery import register_jobs
 
-from demo_git_datasource.jobs.data_quality import (
+from .data_quality import (
     VerifyHostnames, VerifyPlatform, VerifyPrimaryIP, VerifyHasRack, VerifyCircuitTermination
 )
 
